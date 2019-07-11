6f7bf65f5ccb0411b67329aed119d89176b0c886
Katie Wilday <wildayk@gmail.com>
2019-07-11 19:20:26 -0400

    fixed views folder name

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :instructors, only: [:show]
     resources :clients, only: [:index, :show, :new, :create, :edit]
-
-    get 'clients/:id/client_data', to: 'clients#client_data'
+    root to: "instructors#index"
+    #get 'clients/:id/client_data', to: 'clients#client_data'
 end
