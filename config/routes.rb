83b114f2c6a46a7a15ad4bf19121cef1612145c8
Katie Wilday <wildayk@gmail.com>
2019-07-19 16:27:44 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :instructors
-    resources :clients
+    resources :instructors, only: [:new, :create, :show, :edit, :destroy]
+    resources :clients, only: [:new, :show]
+    resources :users, only: [:new, :create, :show, :edit, :destroy]
     root to: "instructors#welcome"
 
     get '/custom_query', to: 'clients#custom_query'
     get 'clients/:id/next', to: 'clients#next'
+    get '/signin', to: 'sessions#new'
+    post '/signin', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
     #get 'clients/:id/client_data', to: 'clients#client_data'
 end
